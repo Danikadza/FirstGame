822e2a0b8278e2f066488cb6dad57b7a771bfc00
Dmitry Sorokin <sorok-dmitry345@yandex.ru>
2024-08-27 20:13:05 +0300

    Remove xxHash

---
+++ src/Engine/include/GS_ENGINE/EventSystem.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef GS_EVENT_SYSTEM_H
+#define GS_EVENT_SYSTEM_H
+
+#include "Define.h"
+#include <stdbool.h>
+
+
+#endif
--- src/Game/src/Entry.c
+++ src/Game/src/Entry.c
@@ -3,16 +3,13 @@
 
 int main()
 {
-    GS_Window* window = GS_WindowCreate("GS_ENGINE",800,600);
-    printf("%d\n",GS_WindowGetWidth(window));
-    printf("%d\n",GS_WindowGetHeight(window));
-    printf("%s\n",GS_WindowGetTitle(window));
 
-   GS_WindowSetWidth(window,1280);
 
-    while (!GS_WindowShouldClose(window))
-    {
-        GS_WindowPollEvents(window);
-    }
-    GS_WindowDestroy(&window);
+    // GS_Window* window = GS_WindowCreate("GS_ENGINE",800,600);
+
+    // while (!GS_WindowShouldClose(window))
+    // {
+    //     GS_WindowPollEvents(window);
+    // }
+    // GS_WindowDestroy(&window);
 }
