dd6418ba71bb53ccca89655943d524ace2701844
Dmitry Sorokin <103883634+Memassik@users.noreply.github.com>
2024-08-28 16:17:43 +0300

    Small Update To Event Manager

--- src/Engine/include/GS_ENGINE/EventManager.h
+++ src/Engine/include/GS_ENGINE/EventManager.h
@@ -4,33 +4,56 @@
 
 #include "Define.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 #define GS_TOTAL_EVENT_TYPES 1024
 
-typedef enum{
-    GS_NullEvent = 0,
-    GS_WindowResizeEvent,
-    GS_UserEvent0 = 256
-}GS_EventType;
-
-typedef union 
+typedef enum
 {
-    unsigned int ui[4];
-    int i[4];
-}GS_EventContext;
+    GS_NullEvent = 0,
+    GS_WindowCloseEvent,
+    GS_WindowNameChangedEvent,
+    GS_WindowResizeEvent,
+    GS_WindowFocusEvent,
+    GS_WindowUnfocusEvent,
+    GS_MouseMovedEvent,
+    GS_MouseScrolledEvent,
+    GS_MouseButtonPressedEvent,
+    GS_MouseButtonReleasedEvent,
+    GS_KeyPressedEvent,
+    GS_KeyReleasedEvent,
+    GS_UserEvent0 = 256
+} GS_EventType;
 
-typedef bool (*GS_EventCallback)(GS_EventType type,void* sender,void* listener, GS_EventContext ctx);
+typedef union
+{
+    uint64_t ui64[4];
+    int64_t i64[4];
+    uint32_t ui32[8];
+    int32_t i32[8];
+    uint16_t ui16[16];
+    int16_t i16[16];
+    uint8_t ui8[32];
+    int8_t i8[32];
+    float f32[8];
+    double f64[4];
+    char c[32];
+} GS_EventContext;
+
+typedef bool (*GS_EventCallback)(GS_EventType type, void *sender,
+                                 void *listener, GS_EventContext ctx);
 typedef struct GS_EventManager GS_EventManager;
 
-extern GS_EventManager* gsEventManager;
+extern GS_EventManager *gsEventManager;
 
 GS_API void GS_EventManagerCreate();
 GS_API void GS_EventManagerDestroy();
 
-GS_API void GS_EventManagerRegisterCallback(GS_EventType type,void* listener, GS_EventCallback callback);
-GS_API void GS_EventManagerUnregisterCallback(GS_EventType type,void* listener, GS_EventCallback callback);
-GS_API void GS_EventManagerFireEvent(GS_EventType type,void* sender, GS_EventContext ctx);
-
-
+GS_API void GS_EventManagerRegisterCallback(GS_EventType type, void *listener,
+                                            GS_EventCallback callback);
+GS_API void GS_EventManagerUnregisterCallback(GS_EventType type, void *listener,
+                                              GS_EventCallback callback);
+GS_API void GS_EventManagerFireEvent(GS_EventType type, void *sender,
+                                     GS_EventContext ctx);
 
 #endif
