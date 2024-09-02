a0ef7c8821111139b1c5481b4df4fffce66a0853
Dmitry Sorokin <sorok-dmitry345@yandex.ru>
2024-09-02 21:58:12 +0300

    Removed Renderer

--- src/Game/src/Entry.c
+++ src/Game/src/Entry.c
@@ -1,15 +1,11 @@
 #include <GS_ENGINE/EventManager.h>
 #include <GS_ENGINE/Logger.h>
-#include <GS_ENGINE/Renderer/Renderer.h>
 #include <GS_ENGINE/Window.h>
 
 int main()
 {
     GS_LoggerInitialize();
     GS_EventManagerCreate();
-
-    GS_Renderer *renderer = GS_RendererCreate();
-    GS_RendererDestroy(&renderer);
 
     GS_Window *window = GS_WindowCreate("Oleg LOX", 1280, 720);
     while (!GS_WindowShouldClose(window))
